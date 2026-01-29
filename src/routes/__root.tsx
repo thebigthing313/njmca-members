a8a9401b6b0415ea0c14ef4fe3fbbe22450bad54
thebigthing313 <adriankabigting@gmail.com>
2026-01-28 20:37:00 -0500

    feat: Refactor root route to use context and simplify component structure

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,47 +1,23 @@
-import '../index.css'
-import '../App.css'
+import "../index.css";
+import "../App.css";
 
-import { useState } from 'react'
+import { QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+interface MyRouteContext {
+  queryClient: QueryClient;
+  supabase: SupabaseClient;
+}
+export const Route = createRootRouteWithContext<MyRouteContext>()({
+  component: App,
+});
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-
-export const Route = createRootRoute({
-  component: () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [queryClient] = useState(
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: 3,
-          },
-        },
-      })
-    )
-    return (
-      <>
-        <div className='p-2 flex gap-2 absolute top-0 left-0 w-full border-b-2 border-white'>
-          <Link to='/' className='[&.active]:text-orange-500'>
-            Home
-          </Link>{' '}
-          <Link to='/query' className='[&.active]:text-orange-500'>
-            Query
-          </Link>
-          <Link to='/form' className='[&.active]:text-orange-500'>
-            Form
-          </Link>
-          <Link to='/table' className='[&.active]:text-orange-500'>
-            Table
-          </Link>
-        </div>
-        <QueryClientProvider client={queryClient}>
-          <div className='mt-32'>
-            <Outlet />
-          </div>
-        </QueryClientProvider>
-        <TanStackRouterDevtools />
-      </>
-    )
-  },
-})
+function App() {
+  return (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+}
